fe415c74b94ceae9211ae974023acb91398fe487
Paul Ahern <ahernp@ahernp.com>
2018-07-10 13:19:49 +0100

    Show recently viewed pages in sidebar

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,10 +1,11 @@
 from collections import OrderedDict
 
-from mpages.models import Page
+from mpages.models import Page, PageRead
 
 
 def menus(request):
     main_menu = Page.objects.get(slug="main-menu")
+
     recent_updates = Page.objects.all().order_by("-updated")[:10]
     updates = OrderedDict()
     for page in recent_updates:
@@ -12,8 +13,20 @@ def menus(request):
         if update_date not in updates:
             updates[update_date] = {"date": update_date, "pages": []}
         updates[update_date]["pages"].append(page)
+
+    page_reads = PageRead.objects.all()[:50]
+    recent = []
+    for page_read in page_reads:
+        if page_read.page not in recent:
+            recent.append(page_read.page)
+        if len(recent) == 10:
+            break
+
     return {
         "mainmenu": main_menu.content_as_html,
-        "sidebar": {"updates": list(updates.values())},
+        "sidebar": {
+            "updates": list(updates.values()),
+            "recent": recent
+        },
         "request": request,
     }
