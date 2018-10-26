a474bff624169f71e02de5678b59ef72f25f368b
Paul Ahern <ahernp@ahernp.com>
2018-10-26 16:33:08 +0100

    Added full-page version of timers

--- timers/urls.py
+++ timers/urls.py
@@ -1,6 +1,8 @@
-
 from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.TimersListView.as_view(), name="timers")]
+urlpatterns = [
+    path("", views.TimerListView.as_view(), name="timers"),
+    path("<int:pk>/", views.TimerDetailView.as_view(), name="timer-detail"),
+]
--- timers/views.py
+++ timers/views.py
@@ -1,7 +1,12 @@
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from .models import Timer
 
 
-class TimersListView(ListView):
+class TimerDetailView(DetailView):
+    model = Timer
+
+
+class TimerListView(ListView):
     model = Timer
