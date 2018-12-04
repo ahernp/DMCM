0943f4ef893582ba6b7c596583839d50049108b5
Paul Ahern <ahernp@ahernp.com>
2018-12-04 10:03:02 +0000

    Tidied history context processor.

--- core/context_processors.py
+++ core/context_processors.py
@@ -5,11 +5,13 @@ from django.db.models import Count
 
 from mpages.models import Page, PageRead
 
+NUMBER_OF_PAGES_IN_HISTORY = 6
+
 
 def menus(request):
     main_menu = Page.objects.get(slug="main-menu")
 
-    recent_updates = Page.objects.all().order_by("-updated")[:5]
+    recent_updates = Page.objects.all().order_by("-updated")[:NUMBER_OF_PAGES_IN_HISTORY-1]
     updates = OrderedDict()
     for page in recent_updates:
         update_date = page.updated.strftime("%Y-%m-%d")
@@ -17,12 +19,12 @@ def menus(request):
             updates[update_date] = {"date": update_date, "pages": []}
         updates[update_date]["pages"].append(page)
 
-    page_reads = PageRead.objects.all()[:50]
+    page_reads = PageRead.objects.all()
     recent = []
     for page_read in page_reads:
         if page_read.page not in recent:
             recent.append(page_read.page)
-        if len(recent) == 6:
+        if len(recent) == NUMBER_OF_PAGES_IN_HISTORY-1:
             break
 
     popular = (
@@ -30,14 +32,14 @@ def menus(request):
         .exclude(page__slug=settings.TASK_LIST_SLUG)
         .values("page__slug", "page__title")
         .annotate(total=Count("page__slug"))
-        .order_by("-total", "page__slug")[:5]
+        .order_by("-total", "page__slug")[:NUMBER_OF_PAGES_IN_HISTORY-1]
     )
 
     return {
         "mainmenu": main_menu.content_as_html,
         "sidebar": {
             "updates": list(updates.values()),
-            "recent": recent[1:],
+            "recent": recent,
             "popular": popular,
         },
         "request": request,
