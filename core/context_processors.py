1698c646f9999d99c296ae71ebf3fec100c2921e
Paul Ahern <ahernp@ahernp.com>
2019-05-01 21:36:03 +0100

    Show last 20 pages in history

--- core/context_processors.py
+++ core/context_processors.py
@@ -16,7 +16,7 @@ def menus(request):
     for page_read in page_reads:
         if page_read.page not in history:
             history.append(page_read.page)
-        if len(history) == NUMBER_OF_PAGES_IN_HISTORY - 1:
+        if len(history) == NUMBER_OF_PAGES_IN_HISTORY:
             break
 
     return {
